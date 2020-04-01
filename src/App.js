4878e7f5075e13acf82fbbb952419fa3d6098424
M4s4t0 <marco.sch4064@gmail.com>
2020-04-01 15:45:20 +0200

    created a ContactList file that duplicate the Contact file withe the users props in it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Contact from "./component/Contact";
+import ContactList from './component/ContactList'
 
 function App() {
   return (
     <div className="App">
-      <Contact name="Rodney Mullen" avatar="https://randomuser.me/api/portraits/men/74.jpg" online/>
-      <Contact name="Colleen Fox" avatar="https://randomuser.me/api/portraits/women/11.jpg" offline/>
-      <Contact name="Deaan Kelly" avatar="https://randomuser.me/api/portraits/women/75.jpg" online/>
+      <ContactList />
     </div>
   );
 }
--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,32 +3,28 @@ import "./Style.css";
 import PropsTypes from 'prop-types'
 
 
-let status = false;
+// let status = false;
 
-const StatusText = stat => {
-  if (stat === true) {
-    return "online";
-  } else {
-    return "offline";
-  }
-};
+// const StatusText = stat => {
+//   if (stat === true) {
+//     return "online";
+//   } else {
+//     return "offline";
+//   }
+// };
 
-const Contact = (props) => {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.name}/>
-      <div className="name">
-        <p>{props.name}</p>
-        <div className="status">
-          <div className={props.online ? "status-online" : "status-offline"}></div>
-          <div className="status-text">
-            <p>{StatusText(status)}</p>
-          </div>
-        </div>
+const Contact = props => (<div className="Contact">
+  <img className="avatar" src={props.avatar} alt={props.name} />
+  <div className="name">
+    <p>{props.name}</p>
+    <div className="status">
+      <div className={props.online ? "status-online" : "status-offline"}></div>
+      <div className="status-text">
+        <p>{props.text}</p>
       </div>
     </div>
-  );
-};
+  </div>
+</div>);
 
 
 
